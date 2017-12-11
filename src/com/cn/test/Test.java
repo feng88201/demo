20082e8ee7fea545de7b8410f1fd995a4ddef484
feng88201 <Administrator@PC-20160603MBXH>
2017-12-11 10:33:48 +0800

    首次提交

---
+++ src/com/cn/test/Test.java
@@ -0,0 +1,11 @@
+package com.cn.test;
+
+import java.util.Date;
+
+public class Test {
+	public static void main(String[] args) {
+		int a = 1;
+		System.out.println("Time is "+new Date());
+	}
+
+}
